0235faa3d8c9e64ad2b83db68e6fbc651e083e73
Mathew Levasseur <matlev91@gmail.com>
2014-10-07 20:57:42 -0700

    Database connection and query API
    Added a file for simplified DB connection and queries.  Configure the
    DBPROP file, include the DBCON file into the file you want to use it in,
    call connect() and query() and you're on your way.
    
    Read the READ ME for instructions and a demo.

---
+++ DB Connector/DBPROP.php
@@ -0,0 +1,6 @@
+<?php
+	define('DB_HOST', ''); // The host url for the database
+	define('DB_USER', ''); // Database user login
+	define('DB_PASS', ''); // Database password
+	define('DB_NAME', ''); // Database name
+?>
